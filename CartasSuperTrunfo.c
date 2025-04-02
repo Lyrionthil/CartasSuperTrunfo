46f13ccc77c4e728a88a20e4ce796bca24b7f61a
Lyrionthil <weliton.rayner@outlook.com>
2025-04-02 14:01:52 +0000

    Realizando as declarações

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,13 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+  char pais1;
+  char codigo1[5];
+  char nomeCidade1[50];
+  int populacao1;
+  float area1;
+  float pib1;
+  int pontosTuristicos1;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
